a0158e8ef71b3146851fc0053c67a218e54d80ab
Roman Haffarov <jiupaladin@gmail.com>
2025-01-23 08:06:39 +0100

    navbar has changed.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -17,7 +17,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 
 const drawerWidth = 240;
-const navItems = [["I'm an Expert", 'expertise'], ['Experiences', 'history'], ['Projects', 'projects'], ['Contact', 'contact'], ['Resume', 'resume']];
+const navItems = [["I'm an Expert", 'expertise'], ['Experiences', 'history'], ['Projects', 'projects'], ['Resume', 'resume'], ['Contact', 'contact']];
 
 function Navigation({parentToChild, modeChange}: any) {
 
