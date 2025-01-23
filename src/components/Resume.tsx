806ccbd65dddea8ab6694f3c5c464073eb1dbb86
Roman Haffarov <jiupaladin@gmail.com>
2025-01-23 07:32:06 +0100

    Updates

--- src/components/Resume.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-function Resume() {
-  return (
-    <a id="resume" aria-label="Open Roman's resume in a new tab" href="https://jiupaladin.github.io/Roman_Haffarov_Resume.pdf" target="_blank"><span>Resume</span></a>
-  );
-}
-
-export default Resume;
